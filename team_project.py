a29bb8c400ca1c5a8b99770cd566ab6ba0ee21dd
Yhoundeh <aarond96@byui.edu>
2021-11-10 22:07:04 -0700

    Moved interface, upload music, add padding

--- team_project.py
+++ team_project.py
@@ -7,7 +7,6 @@ from stats import Stats
 from pygame.draw import rect
 from character import Character
 from randQuestion import question, checkSolution
-from interface import interface
 from sound import sounds
 from CHARACTER_VARIABLES import *
 from GAME_VARIABLES import *
@@ -103,8 +102,8 @@ def gameWindow():
     win.fill((0, 0, 0))
     win.blit(bg, (0,0))
     
-    red_stats.show_health_bar(win, red.rect.x, red.rect.y)   
     sprite_group.draw(win)
+    red_stats.show_health_bar(win, red.rect.x, red.rect.y)   
     hydra.showCharacter(win, i)
     hydra_stats.show_health_bar(win, hydra.rect.x + 35, hydra.rect.y)
     eye_stats.show_health_bar(win, eye.rect.x + 5, eye.rect.y)
@@ -113,6 +112,33 @@ def gameWindow():
     skeleton_stats.show_health_bar(win, skeleton.rect.x + 15, skeleton.rect.y)
 
     i += 1
+
+def interface(win, rect, text, levelText, input_rect, user_text, color, base_font):
+
+   # copying the text surface object to the display surface object 
+    # at the center coordinate.
+    textRect = text.get_rect()
+
+    # draw rectangle and argument passed which should
+    # be on screen
+    rect(win, color, input_rect)
+
+    # set the center of the text rectangle object.
+    textRect.center = (TEXT_X, TEXT_Y)
+
+    text_surface = base_font.render(user_text, True, (255, 255, 255))
+
+    # render at position stated in arguments
+    win.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+    win.blit(levelText, (LEVEL_X, LEVEL_Y))
+    win.blit(text, textRect)
+
+    # set width of textfield so that text cannot get outside of user's text input
+    # Origionally set to 100? Why does this exist?
+    input_rect.w = max(INPUT_WIDTH, text_surface.get_width()+10)
+
+    # The one and true update
+    pygame.display.update()
 
 run = True
 which_color = 0
